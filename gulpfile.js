b52cb42a6347359567b78c023faacfa1e685994d
Yudi <madewahyudi22@gmail.com>
2021-08-11 15:50:07 +0800

    init

---
+++ gulpfile.js
@@ -0,0 +1,59 @@
+// gulpfile.js
+const {watch, series, src, dest} = require("gulp");
+const browserSync = require("browser-sync").create();
+const htmlmin = require('gulp-htmlmin');
+const postcss = require("gulp-postcss");
+const imagemin = require("gulp-imagemin");
+
+// Task for minifying html file
+function htmlminTask (cb) {
+  src("./src/*.html")
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(dest('./public'))
+    .pipe(browserSync.stream());
+  cb();
+}
+
+// Task for compiling CSS files using PostCSS
+function cssTask (cb) {
+  src("./src/css/*.css")
+    .pipe(postcss())
+    .pipe(dest("./public/css"))
+    .pipe(browserSync.stream());
+  cb();
+}
+
+// Task for minifying images
+function imageminTask (cb) {
+  src("./src/images/*")
+    .pipe(imagemin())
+    .pipe(dest("./public/images"));
+  cb();
+}
+
+// Serve from browserSync server
+function browsersyncServe (cb) {
+  browserSync.init({
+    server: {
+      baseDir: "./public",
+    },
+  });
+  cb();
+}
+
+function browsersyncReload (cb) {
+  browserSync.reload();
+  cb();
+}
+
+// Watch Files & Reload browser after tasks
+function watchTask () {
+  watch("./src/*.html", series(htmlminTask, browsersyncReload));
+  watch("./src/css/*.css", series(cssTask, browsersyncReload));
+}
+
+// Default Gulp Task
+exports.default = series(htmlminTask, cssTask, browsersyncServe, watchTask);
+exports.html = htmlminTask;
+exports.css = cssTask;
+exports.images = imageminTask;
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  mode: "jit",
+  purge: [
+    './src/*.html',
+    './src/**/*.scss'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
