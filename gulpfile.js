8008c2048ae5deca00b4a70a762aa3451139cd74
Yudi <madewahyudi22@gmail.com>
2021-09-28 13:38:33 +0800

    update gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const {watch, series, src, dest} = require("gulp");
 const browserSync = require("browser-sync").create();
 const htmlmin = require('gulp-htmlmin');
 const postcss = require("gulp-postcss");
+const sass = require('gulp-sass')(require('sass'));
 const imagemin = require("gulp-imagemin");
 const minify = require("gulp-minify");
 
@@ -14,12 +15,13 @@ function htmlminTask (cb) {
   cb();
 }
 
-// Task for compiling CSS files using PostCSS
-function cssTask (cb) {
-  src("./src/css/*.css")
-    .pipe(postcss().on('error', console.log))
-    .pipe(dest("./public/css"))
-    .pipe(browserSync.stream());
+// Task for compiling CSS files 
+function styleTask (cb) {
+  src("./src/sass/app.scss")
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(dest("./src/dist/css"))
+    .pipe(postcss())
+    .pipe(dest("./public/css"));
   cb();
 }
 
@@ -57,8 +59,8 @@ function browsersyncReload (cb) {
 
 // Watch Files & Reload browser after tasks
 function watchTask () {
-  watch("./src/*.html", series(htmlminTask, cssTask, browsersyncReload));
-  watch("./src/css/*.css", series(cssTask, browsersyncReload));
+  watch("./src/*.html", series(htmlminTask, styleTask, browsersyncReload));
+  watch("./src/sass/*.scss", series(styleTask, browsersyncReload));
   watch("./src/js/*js", series(jsTask, browsersyncReload));
   watch("./src/images", series(imageminTask));
 }
@@ -66,7 +68,7 @@ function watchTask () {
 // Default Gulp Task
 exports.default = series(
   htmlminTask,
-  cssTask,
+  styleTask,
   jsTask,
   imageminTask,
   browsersyncServe,
